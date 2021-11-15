f24dbaa699400a565558187b3db8f4568ee8844d
Maxim Mochalkin <maksimmochalkin@gmail.com>
2021-11-15 12:46:26 +0600

    add some changes

--- AdvancedCSharp/Program.cs
+++ AdvancedCSharp/Program.cs
@@ -12,25 +12,18 @@
         static void Main(string[] args)
         {
             var path = "C:\\Users\\Maxim_Mochalkin\\source\\repos\\For course\\Part3\\AdvancedCSharp\\AdvancedCSharpLibrariesTests\\TestsData\\";
-
-            var del = new FiltrationEventHandler(() => 
+            var del = new FiltrationEventHandler(list =>
             {
-                string[] files = Directory.GetFiles(path, "*", SearchOption.AllDirectories);
-                var result = new List<(string fileName, string folderName)>();
-                foreach (var file in files)
-                {
-                    var fi = new FileInfo(file);
-                    var name = fi.Name;
-                    var dirname = fi.Directory.Name;
-                    result.Add((name, dirname));
-                }
+                var result = list.Where(item => item.fileName.Contains(".xlsx"))
+                .Select(item => item).ToList();
 
                 return result;
             });
-            
-            var FSV = new FileSystemVisitor(del);
-            var result = FSV.LaunchEvent();
-            
+
+            var FSV = new FileSystemVisitor(path, del);
+            var result = FSV.GetFilesAndFoldersWithoutFiltration();
+            var filterResult = FSV.GetFilesAndFoldersWithFiltration();
+           
         }
 
         
--- AdvancedCSharpLibraries/FileSystemVisitor.cs
+++ AdvancedCSharpLibraries/FileSystemVisitor.cs
@@ -2,34 +2,37 @@
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.IO;
+
     public class FileSystemVisitor
     {
+        private readonly string _path;
 
-        public delegate List<(string fileName, string folderName)> FiltrationEventHandler();
+        public delegate List<(string fileName, string folderName)> FiltrationEventHandler(List<(string fileName, string folderName)> filesAndFolders);
         private event FiltrationEventHandler _filtrationEvent;
 
         private delegate void StartOrFinishAction(string message);
         private event StartOrFinishAction _startOrFinishEvent;
 
-        public FileSystemVisitor(FiltrationEventHandler eventHandler)
+        public FileSystemVisitor(string path, FiltrationEventHandler eventHandler)
         {
+            _path = path;
             _filtrationEvent = eventHandler;
         }
 
-        public List<(string fileName, string folderName)> LaunchEvent()
+        public List<(string fileName, string folderName)> GetFilesAndFoldersWithoutFiltration()
         {
             if (_filtrationEvent != null)
             {
                 _startOrFinishEvent += GenerateMessage;
                 _startOrFinishEvent.Invoke("Start working");
-                var result = _filtrationEvent.Invoke();
-                foreach (var item in result)
+                var filesAndFolders = GetFilesAndFolders();
+                foreach (var item in filesAndFolders)
                 {
                     Console.WriteLine(item);
                 }
                 _startOrFinishEvent.Invoke("Finish working");
-                return result;
+                return filesAndFolders;
 
             }
             else
@@ -40,6 +43,45 @@
             }
 
 
+        }
+
+        public List<(string fileName, string folderName)> GetFilesAndFoldersWithFiltration()
+        {
+            if (_filtrationEvent != null)
+            {
+                _startOrFinishEvent += GenerateMessage;
+                _startOrFinishEvent.Invoke("Start filtration");
+                var filesAndFolders = GetFilesAndFolders();
+                var result = _filtrationEvent.Invoke(filesAndFolders);
+                foreach (var item in result)
+                {
+                    Console.WriteLine(item);
+                }
+                _startOrFinishEvent.Invoke("Finish filtration");
+                return filesAndFolders;
+
+            }
+            else
+            {
+                _startOrFinishEvent += GenerateMessage;
+                _startOrFinishEvent.Invoke("Filtration function not found");
+                return null;
+            }
+        }
+
+        private List<(string fileName, string folderName)> GetFilesAndFolders()
+        {
+            string[] files = Directory.GetFiles(_path, "*", SearchOption.AllDirectories);
+            var result = new List<(string fileName, string folderName)>();
+            foreach (var file in files)
+            {
+                var fi = new FileInfo(file);
+                var name = fi.Name;
+                var dirname = fi.Directory.Name;
+                result.Add((name, dirname));
+            }
+
+            return result;
         }
 
         private void GenerateMessage(string message)
